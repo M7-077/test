1056785b12aa3accc29b85facf4e2c0fe11818f9
xCReePeRMax999 <51760076+xCReePeRMax999@users.noreply.github.com>
2019-07-08 19:40:03 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
-const devs = ['389090790984515594'];
+const devs = ['462949947680096257'];
 const db = require('quick.db');
-const premium = ['470896018603376640']
+const premium = ['462949947680096257']
 const client = new Discord.Client();   
 const bot = new Discord.Client();   
 const giphy = require('giphy-api')();    
